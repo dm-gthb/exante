4779a1a4ef4ca36cda8f7aeb3e37610c174d08e2
Dmitrii Mamonov <dmitry.mamonov@justwebit.ru>
2017-03-21 20:44:48 +0300

    add fonts

--- src/fonts/font_loading.js
+++ src/fonts/font_loading.js
@@ -79,3 +79,15 @@ loadFont(
     '/css/fonts_woff.css?v=1',   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF
     '/css/fonts_woff2.css?v=1'   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF2
   );
+
+loadFont(
+    'Fedra Sans',                      // название шрифта
+    '/css/fonts_woff.css?v=1',   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF
+    '/css/fonts_woff2.css?v=1'   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF2
+  );
+
+loadFont(
+    'Gill Sans',                      // название шрифта
+    '/css/fonts_woff.css?v=1',   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF
+    '/css/fonts_woff2.css?v=1'   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF2
+  );
