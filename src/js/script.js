f1947b5c6e783f33d69c8830617bed3453e48276
dm-gthb <indexdm@yandex.ru>
2017-03-26 19:05:14 +0300

    add script for toggler

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-  // весь ваш код
-});
+var toggler = document.getElementById('toggler');
+toggler.addEventListener('click', activateNav);
+function activateNav(e) {
+  e.preventDefault();
+  document.getElementById('main-nav').classList.toggle('main-nav--active');
+}
